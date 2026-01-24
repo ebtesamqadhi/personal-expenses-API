d33a9cc9ef6fb87c0c5a456871dc28bd4375b6d9
ebtesamqadhi <ebtesamqadhi@gmail.com>
2026-01-24 21:27:17 +0300

    create Transactions models

--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from accounts.models import CustomUser
 
 # Create your models here.
+
+class Transaction(models.Model):
+    TRANSACTION_TYPE = (
+        ('income', 'Income'),
+        ('expense', 'Expense'),
+    )
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    type = models.CharField(max_length=10, choices= TRANSACTION_TYPE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True)
+    date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.type}"

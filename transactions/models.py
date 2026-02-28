0aca6ed9984c4723045277d7175db76df3cda951
ebtesamqadhi <ebtesamqadhi@gmail.com>
2026-03-01 02:01:31 +0300

    Create Category model

--- transactions/models.py
+++ transactions/models.py
@@ -1,14 +1,25 @@
 from django.db import models
 from accounts.models import CustomUser
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
 
+class Category(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
 class Transaction(models.Model):
     TRANSACTION_TYPE = (
         ('income', 'Income'),
         ('expense', 'Expense'),
     )
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, null=True, blank=True)
     type = models.CharField(max_length=10, choices= TRANSACTION_TYPE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True)
